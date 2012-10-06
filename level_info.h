7b24d86cc6b10e61bb82c0750ed2a8e7f12984e2
Siyuan Liu <siyupku@gmail.com>
2012-10-06 15:40:54 +0800

    修改level_info

--- level_info.h
+++ level_info.h
@@ -16,13 +16,13 @@ using namespace std;
 class level_info
 {
 	public:
-	level_info(ElevatorParameters* , StorageManager*);         // 使用定义初始化楼层信息
-	void generate_passager();                // 在一个时间片以后将得到的新乘客连入列表
+	level_info(ElevatorParameters* , StorageManager*);       // 使用定义初始化楼层信息
+	void generate_passenger();                               // 在一个时间片以后将得到的新乘客连入列表
 	
-	ElevatorParameters* transcript_elev;     // 楼层对定义中变量的副本
-	Passenger* pass_head; 	                 // 每层楼乘客的链表
-	StorageManager* level_stomanger;         // 后面调用其中的函数得到新的乘客连接到楼层乘客链表中
-	poisson culculate_poisson;
+	ElevatorParameters* transcript_elev;                     // 定义中变量的副本
+	Passenger** pass_head; 	                                 // 楼层乘客的链表
+	StorageManager* level_stomanger;                         // 后面调用其中的函数得到新的乘客连接到楼层乘客链表中
+	poisson culculate_poissona
 };
 
 #endif
