a801e1ba771e1ec0215bfc50ad8956100da9bc5e
Will Yang <wkyjyy@gmail.com>
2012-10-06 22:09:51 +0800

    new main.cpp

--- main.cpp
+++ main.cpp
@@ -6,81 +6,15 @@
 #include "poisson.h"
 
 int main()
-<<<<<<< HEAD
-{/*
-<<<<<<< HEAD
-	int a=0;
-=======
-=======
 {
->>>>>>> 336794ab533e3cdc4def357d6921f72a27b828d7
-    int a=0;
-    int b = 1;
-    printf("yeah, it works!\n%d\n", a+b);
-    StorageManager * m = new StorageManager(5);
-    Passenger* ptr = m->get_new();
-    printf("m:0x%x, ptr:0x%x\n", m, ptr);
-    ptr = m->get_new();
-    printf("ptr:0x%x\n", ptr);    
-    ptr = m->get_new();
-    printf("ptr:0x%x\n", ptr);    
-    ptr = m->get_new();
-    printf("ptr:0x%x\n", ptr);    
-    ptr = m->get_new();
-    printf("ptr:0x%x\n", ptr);    
-    ptr = m->get_new();
-    printf("ptr:0x%x\n", ptr);    
-    m->push_arrived(ptr);
-    ptr = m->get_new();
-    printf("ptr:0x%x\n", ptr);    
-    m->push_arrived(ptr);
-    ptr = m->get_new();
-    printf("ptr:0x%x\n", ptr);    
-    m->push_arrived(ptr);
-    delete m;
-    */
-    ElevatorParameters e;
-    e.arrival_rate = 0.8;
-    poisson p(&e);
-    cout << p.get_next() << endl;
-    StorageManager s;
-    level_info l(&e, &s);
-    l.generate_passenger();
-    return 0;
-    /*
-=======
-=======
->>>>>>> 336794ab533e3cdc4def357d6921f72a27b828d7
-    
     ElevatorParameters * param = new ElevatorParameters;
     bool input_res = get_param(argc, argv, param);
     if (!input_res)
         return 0;
-    printf("param: %d,%d,%d,%d,%lf,%d,%s,%d,%s\n",
-           param->elevator_num,
-           param->elevator_speed,
-           param->elevator_load,
-           param->level_num,
-           param->arrival_rate,
-           param->simulation_cycles,
-           param->output_file,
-           param->algorithm_select,
-           param->verbose?"yes":"no");
-
-    poisson p(param);
-    cout << p.get_next() << endl;
-    cout << p.get_next() << endl;
-    cout << p.get_next() << endl;
-    cout << p.get_next() << endl;
-    cout << p.get_next() << endl;
-    cout << p.get_next() << endl;
-    cout << p.get_next() << endl;
-    cout << p.get_next() << endl;    
-    cout << p.get_next() << endl;
-    cout << p.get_next() << endl;
-
-
+    StorageManager * m = new StorageManager();
+    level_info *levels = new level_info(param, m);
+    
     return 0;
-*/
+    
 }
 // sob: Weikun "William" Yang wkyjyy@gmail.com
