704f02b26d98dc66ec0054be245ba08bad57568b
Siyuan Liu <siyupku@gmail.com>
2012-10-06 16:47:00 +0800

    修改poisson

--- poisson.cpp
+++ poisson.cpp
@@ -18,21 +18,24 @@ poisson::poisson(ElevatorParameters* in_para)
 double poisson::poisson_function(int k)
 {
 	double factorial = 1;
-	for(int i = 1; i <= k; i ++)
+
+	for(int i = 1; i <= k; i ++) // 阶乘
 		factorial = factorial*i;
+	
 	return pow(poisson_para.arrival_rate, k * 1.0) * pow(M_E, -poisson_para.arrival_rate) 
-		/factorial;
+		/factorial; //泊松公式
 }
 
 int poisson::get_new()
 {
-	srand( (unsigned)time(0) );
-	double select_p = rand()/(double)(RAND_MAX);
-	double P = 0;
+	srand( (unsigned)time(0) ); // 以系统时间为种子
+	double select_p = rand()/(double)(RAND_MAX); // 产生0~1的随即浮点数
+	
+	double P = 0; // 累计概率，使概率分布具有几何性质
 	for(int i = 0; i < sum_area; i ++)
 	{
 		if(P > select_p)
 			return i-1;
-		P = P + distribution[i];
+		P = P + distribution[i]; 
 	}
 }
--- poisson.h
+++ poisson.h
@@ -21,8 +21,8 @@ class poisson
 {
 	public:
 	poisson(ElevatorParameters*);
-	double poisson_function(int k);
-	int get_next();
+	double poisson_function(int k); // 构造泊松公式，计算有限个点的概率
+	int get_next(); // 由得到的泊松分布，产生随即数，确定新乘客的个数
 	
 	private:
 	ElevatorParameters* poisson_para;
