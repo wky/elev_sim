ed45dddefc02697dd79442d1e0d62edde08be96c
Will Yang <wkyjyy@gmail.com>
2012-10-06 14:26:17 +0800

    添加storage.h

---
+++ storage.h
@@ -0,0 +1,25 @@
+/*
+storage.h
+管理乘客的存储
+*/
+#ifndef __storage_h
+#define __storage_h
+
+/*
+StorageManager类
+单链表的内存池管理乘客
+*/
+class StorageManager{
+    private:
+        Passenger *head;
+        Passenger *tail;
+        void allocate_more(int amount = 50);
+    public:
+        StorageManager(int init_vol = 512);
+        Passenger* get_new();               //获取新乘客(数据未初始化)
+        void push_arrived(Passenger *);     //回退到达后的乘客并统计信息
+        ~StorageManager();
+}
+
+#endif
+
